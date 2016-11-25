0e186c48aeb87331806cd6289b1c8b1f78cb1673
Андрей Кашкан < >
2016-11-25 16:37:10 +0300

    create fullpage slider: create inner slider and sliding elemttns

--- bitrix/templates/main/js/common.js
+++ bitrix/templates/main/js/common.js
@@ -61,33 +61,69 @@ function FullSlider(el, config) {
 	extend( this.options, config );
 
 	this.conf = {
+		current: {
+			slide: 0,
+			slideInner: 0
+		},
 		classes: {
 			next: "next",
 			prev: "prev",
 			prevClass: "slideOut",
 			nextClass: "slideIn",
-			activeCLass: "current"
+			activeCLass: "current",
+			activeCLassTransform: "active",
+			prevClassTransform: "transform-out",
+			nextClassTransform: "transform-in"
 		},
 		animationEvents: "mozAnimationEnd MSAnimationEnd oAnimationEnd animationend"
 	}
+
+	this.tw = new TimelineLite();
 
 	this.initDefault()
 };
 
 FullSlider.prototype.initDefault = function(){
+	var self = this;
 	$(this.options.slide).first().addClass("current");
-	$(this.options.innerSlider).children().first().addClass("active");
 	$(this.options.paginBar).children().first().addClass("current");
 	this.currentColor = $(this.options.slide).parent().find(".current img").data("color");
 	$(this.options.colorContainer).css("background-color", this.currentColor)
 	this.slides = $(this.options.slide);
+	// this.slidesInner = $(this.options.innerSlider).find(".slide-inserted");
+
+	$(this.options.slide).find("img").each(function(){
+		this.color = $(this).data("color");
+
+		$(this).parent().find("img").parent().css("background-color", this.color);
+	})
+
+	$(this.options.innerSlider).each(function(){
+		$(this).find(".slide-inserted").first().addClass("active");
+		$(this).find(".slide-inserted").first().next().addClass("slideNext");
+		this.lengthSlide = $(this).find(".slide-inserted").length;
+		self.generateBullets($(this), this.lengthSlide);
+	});
 
 	this.init();
 };
 
+FullSlider.prototype.generateBullets = function(container, length){
+	var self = this;
+
+	for(var i = 0; i < length; i++) {
+		container.find(this.options.innerBullets).append("<span class='bullet' data-bullet-index='" + i + "'></span>")
+	}
+
+	container.find(".bullet").first().addClass("active");
+}
+
 FullSlider.prototype.init = function(){
 	this.paginTrigger = $(this.options.paginBar).children();
 	this.colorContainer = $(this.el).find(".color-container");
+	this.arrowBtn = $(this.options.arrowNext);
+
+	this.bulletItem = $(this.options.innerBullets).find(".bullet");
 
 	this.action = false;
 
@@ -104,46 +140,156 @@ FullSlider.prototype.handlerEvents = function(){
 			return false;
 		}
 
-		self.action = true;
-
 		this.curr = $(self.options.paginBar).find(".current").data("index-pagin");
 		this.next = $(this).data("index-pagin");
 
-
-		self.triggerBullets($(this).data("index-pagin"))
-		self.show(this.curr, this.next);
-		
+		self.triggerNavBullets($(this).data("index-pagin"))
+		self.show(this.curr, this.next, self.slides);
 	});
+
+	this.bulletItem.on("click", function(){
+		if($(this).hasClass("active")) return false;
+
+		if(self.action) {
+			return false;
+		}
+
+		this.curr = $(this).parents(self.options.innerBullets).find(".active").data("bullet-index");
+		this.next = $(this).data("bullet-index");
+
+
+		this.slidesInserted = $(this).parents(self.options.innerSlider).find(".slide-inserted");
+
+		self.triggerBullets($(this))
+		self.show(this.curr, this.next, this.slidesInserted);
+	});
+
+	document.addEventListener("keyup", function(event){
+		if(self.action) {
+			return false;
+		}
+		if( event.keyCode === 38 ) self.prev();
+		if( event.keyCode === 40 ) self.next();
+	});
+
+	this.arrowBtn.on("mouseenter", function() {
+		$(this).parents(self.options.innerSlider).addClass("hovered");
+	});
+	this.arrowBtn.on("mouseleave", function() {
+		if($(this).parents(self.options.innerSlider).hasClass("animation")) return false;
+		$(this).parents(self.options.innerSlider).removeClass("hovered");
+	});
+
+	this.arrowBtn.on("click", function(){
+		$(this).parents(self.options.innerSlider).addClass("animation");
+
+		self.triggerNext($(this).parents(self.options.innerSlider).find(".slide-inserted"))
+	})
+
 };
 
-FullSlider.prototype.triggerBullets = function(index){
+FullSlider.prototype.triggerNext = function(slides){
+	this.cur_slide = this.conf.current.slideInner;
+	this.next_slide = this.cur_slide + 1;
+	this._next_slide = slides[this.next_slide];
+
+
+	if(typeof this._next_slide == "undefined")
+		this.next_slide = 0
+
+	this.show(this.cur_slide, this.next_slide, slides);
+
+
+	
+	this.triggerBullets($("[data-bullet-index=" + $(this.next_slide).index() + "]"))
+}
+
+FullSlider.prototype.prev = function(){
+	this.cur_slide = this.conf.current.slide;
+	this.next_slide = this.cur_slide - 1;
+	this._next_slide = this.slides[this.next_slide];
+
+	if(typeof this._next_slide == "undefined")
+		return false;
+
+	this.show(this.cur_slide, this.next_slide, this.slides);
+	this.triggerNavBullets($(this.next_slide).index());
+}
+
+FullSlider.prototype.next = function(){
+	this.cur_slide = this.conf.current.slide;
+	this.next_slide = this.cur_slide + 1;
+	this._next_slide = this.slides[this.next_slide];
+
+	if(typeof this._next_slide == "undefined")
+		return false;
+
+
+	this.show(this.cur_slide, this.next_slide, this.slides)
+	this.triggerNavBullets($(this.next_slide).index());
+}
+
+FullSlider.prototype.triggerBullets = function(item){
+	item.addClass("active").siblings().removeClass("active");
+};
+
+FullSlider.prototype.triggerNavBullets = function(index){
 	this.paginTrigger.eq(index).addClass("current").siblings().removeClass("current");
 };
 
-FullSlider.prototype.show = function(curr, next) {
+FullSlider.prototype.show = function(curr, next, slides) {
 
-	this.curr_slide = this.slides[curr];
-	this.next_slide = this.slides[next];
+	this.action = true;
 
-	if(!$(this.next_slide).find(".full-slider-inserted")) {
-		this.next_slideColor = $(this.next_slide).find(".active img").data("color");
-	} else {
+	this.curr_slide = slides[curr];
+	this.next_slide = slides[next];
+
+	if($(this.next_slide).parents(".full-slider-inserted").length) {
+		
+		if(next + 1 >= slides.length) {
+			this.next_rotator_slide = slides[0]
+		} else {
+			this.next_rotator_slide = slides[next + 1];
+		}
+
+		$(this.next_rotator_slide).addClass("slideNext").siblings().removeClass("slideNext");
+
 		this.next_slideColor = $(this.next_slide).find("img").data("color");
+
+		$(this.next_slide).addClass(this.conf.classes.activeCLassTransform + " " + this.conf.classes.nextClassTransform).siblings().removeClass(this.conf.classes.activeCLassTransform + " " + this.conf.classes.nextClassTransform);
+		
+		$(this.curr_slide).addClass(this.conf.classes.prevClassTransform).siblings().removeClass(this.conf.classes.prevClassTransform);
+
+		$(this.options.colorContainer).css("background-color", this.next_slideColor);
+
+		this.conf.current.slideInner = next;
+
+	} else {
+		this.next_slideColor = $(this.next_slide).find(".active img").data("color") || $(this.next_slide).find("img").data("color");
+
+		$(this.next_slide).addClass(this.conf.classes.activeCLass + " " + this.conf.classes.nextClass).siblings().removeClass(this.conf.classes.activeCLass + " " + this.conf.classes.nextClass);
+		$(this.curr_slide).addClass(this.conf.classes.prevClass).siblings().removeClass(this.conf.classes.prevClass);
+
+		$(this.options.colorContainer).css("background-color", this.next_slideColor);
+		this.conf.current.slide = next
 	}
 
-	$(this.options.colorContainer).css("background-color", this.next_slideColor);
+	this.animationEnd(slides);
 
-	$(this.next_slide).addClass(this.conf.classes.activeCLass + " " + this.conf.classes.nextClass).siblings().removeClass(this.conf.classes.activeCLass + " " + this.conf.classes.nextClass);
-	$(this.curr_slide).addClass(this.conf.classes.prevClass).siblings().removeClass(this.conf.classes.prevClass);
-
-	this.animationEnd($(this.next_slide))
 	
 };
 
 FullSlider.prototype.animationEnd = function(elem){
+
 	var self = this;
 	$(elem).one("mozAnimationEnd MSAnimationEnd oAnimationEnd animationend", function(){
 		self.action = false;
-		self.slides.removeClass(self.conf.classes.prevClass + " " + self.conf.classes.nextClass)
+		if($(this).parents(".full-slider-inserted").length) {
+			elem.removeClass(self.conf.classes.prevClassTransform + " " + self.conf.classes.nextClassTransform)
+			$(self.options.innerSlider).removeClass("animation hovered");
+			// this.arrowBtn.trigger("mouseenter")
+		} else {
+			elem.removeClass(self.conf.classes.prevClass + " " + self.conf.classes.nextClass)
+		}
 	});
 };
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
 //=include lib/head.js
 //=include lib/jquery.js
+//=include lib/TimelineLite.min.js
 //=include common.js
