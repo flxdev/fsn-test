0096de59027c50af8a06cf9487a43b979c613d44
FLXMD-PC-07\sweel <ak@flxmd.by>
2017-04-18 11:45:22 +0300

    integrated video on main page

--- src/js/common.js
+++ src/js/common.js
@@ -156,7 +156,7 @@ FullSlider.prototype.initDefault = function(){
 	$(this.options.slide).find("img").each(function(){
 		this.color = $(this).data("color");
 
-		$(this).parent().find("img").parent().css("background-color", this.color);
+		$(this).parent().find("img").parents(".slide-image_container").css("background-color", this.color);
 	})
 
 	$(this.options.innerSlider).each(function(){
@@ -1875,4 +1875,66 @@ SimpleValidForm.prototype = {
 	errorEvent: function(inputItem){
 		inputItem.classList.add("error");
 	}	
+}
+function MediaElements(){
+
+    scaleVideoContainer();
+
+    initBannerVideoSize('.video-container .poster img');
+    initBannerVideoSize('.video-container .filter');
+    initBannerVideoSize('.video-container video');
+
+    $(window).on('resize', function() {
+        scaleVideoContainer();
+        scaleBannerVideoSize('.video-container .poster img');
+        scaleBannerVideoSize('.video-container .filter');
+        scaleBannerVideoSize('.video-container video');
+    });
+
+};
+
+function scaleVideoContainer() {
+
+    var height = $(window).height();
+    var unitHeight = parseInt(height) + 'px';
+    $('.homepage-hero-module').css('height',unitHeight);
+
+}
+
+function initBannerVideoSize(element){
+
+    $(element).each(function(){
+        $(this).data('height', $(this).height());
+        $(this).data('width', $(this).width());
+    });
+
+    scaleBannerVideoSize(element);
+
+}
+
+function scaleBannerVideoSize(element){
+
+    var windowWidth = $(element).width(),
+    windowHeight = $(element).height(),
+    videoWidth,
+    videoHeight;
+
+    // console.log(windowHeight);
+
+    $(element).each(function(){
+        var videoAspectRatio = $(this).data('height')/$(this).data('width');
+
+        $(this).width(windowWidth);
+
+        if(windowWidth < 1000){
+            videoHeight = windowHeight;
+            videoWidth = videoHeight / videoAspectRatio;
+            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+
+            $(this).width(videoWidth).height(videoHeight);
+        }
+
+        $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+
+    });
 }
